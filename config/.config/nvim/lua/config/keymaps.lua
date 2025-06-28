18960c5c71a1c2cb178aa84b76043173bc19539a
Paulo Manalo <paulomanalo792@gmail.com>
2025-06-28 17:37:24 -0400

    Updates harpoon to use alt+shift

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -8,12 +8,12 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 local harpoon = require("harpoon")
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<A-S-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<A-!>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<A-@>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<A-#>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<A-$>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<A-S-P>", function() harpoon:list():prev() end)
